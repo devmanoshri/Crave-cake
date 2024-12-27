ae95355b118d33010732a716fac5bfbbb0c3ee23
devmanoshri <manoshri.no@gmail.com>
2024-12-27 11:05:34 +0100

    update: product-details component

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { HeroImageComponent } from '../../components/common/hero-image/hero-image.component';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [HeroImageComponent],
+  imports: [HeroImageComponent, RouterModule],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss',
 })
-export class ProductDetailsComponent {}
+export class ProductDetailsComponent {
+  productID: string = '';
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.productID = params['id'];
+    });
+  }
+}
