a11648d731541c6d44f9ee2f1bcf1da4218def41
devmanoshri <manoshri.no@gmail.com>
2024-12-31 22:24:23 +0100

    feat(page-header): responsive dropdown menu add

--- src/app/components/common/page-header/page-header.component.ts
+++ src/app/components/common/page-header/page-header.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-page-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './page-header.component.html',
   styleUrl: './page-header.component.scss',
 })
-export class PageHeaderComponent {}
+export class PageHeaderComponent {
+  showDropdown = false;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    this.document.addEventListener('click', (event) => {
+      let isTargetInMenu = false;
+      this.document.querySelectorAll('.menu ul, .menu ul *, .hamburger-menu').forEach((el) => {
+        if (el === event.target) {
+          isTargetInMenu = true;
+        }
+      });
+      isTargetInMenu ? (this.showDropdown = true) : (this.showDropdown = false);
+    });
+  }
+}
